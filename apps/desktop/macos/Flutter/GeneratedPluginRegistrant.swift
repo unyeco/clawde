5f9cff7a03f094b107603a613a0d068596ca1901
Aric Camarata <aric.camarata@gmail.com>
2026-04-21 07:45:50 -0400

    chore(desktop): sync Flutter macOS generated files and pubspec.lock
    Regenerated plugin registrant + refreshed lockfile after plugin updates.

--- apps/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import auto_updater_macos
 import file_selector_macos
+import hotkey_manager_macos
 import package_info_plus
 import screen_retriever_macos
 import shared_preferences_foundation
@@ -16,8 +18,10 @@ import webview_flutter_wkwebview
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   AutoUpdaterMacosPlugin.register(with: registry.registrar(forPlugin: "AutoUpdaterMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
